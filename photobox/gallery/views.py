661aa2b285870c2afe0e3b944d95486b4eb9ef64
Charlotte Natasha <natashaonyango97@gmail.com>
2022-05-30 22:03:48 +0200

    defined search

--- photobox/gallery/views.py
+++ photobox/gallery/views.py
@@ -23,4 +23,11 @@ def location(request, pk):
     photos = Image.objects.filter(location=location_id).all()
     context = {'location_id':location_id, 'photos':photos}
 
-    return render(request, 'gallery/index.html', context)     
+    return render(request, 'gallery/index.html', context)
+
+def search(request, pk):
+    if 'category' in request.GET and request.GET("category"):
+        search_term = request.GET.get("category")
+        searched_images = Image.search_by_category(search_term)
+
+        return render(request, 'gallery/index.html', {'photos':searched_images, 'category':search_term})
