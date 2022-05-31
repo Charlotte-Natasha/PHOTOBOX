baaf60a463125b6e6330dddd736d8aecc8721a2b
Charlotte Natasha <natashaonyango97@gmail.com>
2022-05-31 13:40:44 +0200

    chenged folder settings

--- gallery/views.py
+++ gallery/views.py
@@ -6,8 +6,9 @@ def index(request):
 
     photos = Image.objects.all()
     categories = Category.objects.all()
+    locations = Location.objects.all()
 
-    return render(request, 'gallery/index.html', {'photos':photos, 'categories':categories})
+    return render(request, 'gallery/index.html', {'photos':photos, 'categories':categories, 'locations':locations})
 
 def category(request, pk):
 
@@ -23,4 +24,15 @@ def location(request, pk):
     photos = Image.objects.filter(location=location_id).all()
     context = {'location_id':location_id, 'photos':photos}
 
-    return render(request, 'gallery/index.html', context)     
+    return render(request, 'gallery/index.html', context)
+
+def search(request):
+
+    if 'searched' in request.GET and request.GET["searched"]:
+        search_term = request.GET.get("searched")
+        searched_images=Image.search_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'gallery/category.html', {'searched_images':searched_images, 'category':category})
+    else:
+        return render(request, 'gallery/category.html', {})   
